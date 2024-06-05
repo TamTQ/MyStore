daf72d45137c4d428bdf5e20c9884118b10b10a6
TamTQ <tamtqhe181412@fpt.edu.vn>
2024-06-05 22:00:28 +0700

    Add project files.

---
+++ ManageCategories.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore
+{
+    public record Category()
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+    public class ManageCategories
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string ConnectionString = "Server=LAPTOP-IGI0FTD7\\TAMTQ;uid=sa;pwd=123123123;database=MyStore;TrustServerCertificate=True;";
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Select CategoryID, CategoryName from Categories";
+            command = new SqlCommand(SQL, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                if(reader.HasRows == true)
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(new Category
+                        {
+                            CategoryID = reader.GetInt32("CategoryID"),
+                            CategoryName = reader.GetString("CategoryName"),
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally { connection.Close(); }
+            return categories;
+        }
+        public void InsertCategory(Category category) 
+        {
+            connection = new SqlConnection(ConnectionString);
+            command = new SqlCommand("INSERT INTO Categories (CategoryName) VALUES (@CategoryName)", connection);
+            command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally { connection.Close(); }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Update Categories set CategoryName=@CategoryName where CategoryID=@CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+            command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally { connection.Close(); }
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Delete Categories where CategoryID=@CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally { connection.Close(); }
+        }
+    }
+}
